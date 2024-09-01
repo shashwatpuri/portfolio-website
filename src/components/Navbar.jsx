02e8958d998b7921418d9d4c359306f16a072c32
Shashwat <goswamishashwatpuri1@gmail.com>
2024-09-01 21:27:04 +0530

    removed temp button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,8 +37,6 @@ export default function Navbar() {
                 {/* <NavListItem label='Blog' activeTab={activeTab} setActiveTab={setActiveTab} /> */}
                 <NavListItem label='Contact' activeTab={activeTab} setActiveTab={setActiveTab} />
             </ul>
-            <button onClick={()=>{navigate('/a/audiency')}}>Shashwat</button>
-
         </nav>
     )
 }
